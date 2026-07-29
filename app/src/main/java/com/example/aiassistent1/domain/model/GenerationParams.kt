9e35487e3de2617bdb9767ac86f8dd49db3c01f1
Anatoly Batov <play.hoffa@gmail.com>
2026-07-30 01:06:59 +0400

    2048 commit

--- app/src/main/java/com/example/aiassistent1/domain/model/GenerationParams.kt
+++ app/src/main/java/com/example/aiassistent1/domain/model/GenerationParams.kt
@@ -1,7 +1,7 @@
 package com.example.aiassistent1.domain.model
 
 data class GenerationParams(
-    val contextSize: Int = 1_000,
+    val contextSize: Int = 2_048,
     val maxTokens: Int = 128,
     val temperature: Float = 0.35f,
     val topP: Float = 0.8f,
